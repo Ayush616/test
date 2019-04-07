2f74c62f074c116a2d67bd63b17907dc894859f2
Ayush616 <47354211+Ayush616@users.noreply.github.com>
2019-04-07 14:30:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,6 @@
    <?php
 
-$connection = mysqli_connect('shareddb-j.hosting.stackcp.net', 'diary616-3935a18a', 'ayush616', 'diary616-3935a18a');
-
-                if(mysqli_connect_error()){
-
-                    die("Database Connection Error.");
-
-                }  
-
-$query = "SELECT * FROM question";
-
-$result = mysqli_query($connection, $query);
-
-$i = 1;
-while($row = mysqli_fetch_array($result)){
-    echo $i,". ", $row['question']."<br><br>";
-    echo "(A) ", $row['o1']."<br>";
-    echo "(B) ", $row['o2']."<br>";
-    echo "(C) ", $row['o3']."<br>";
-    echo "(D) ", $row['o4']."<br><br>";
-    
-    $i++;
-}
-
-
-
+HIIIIIIII
 
 
 
